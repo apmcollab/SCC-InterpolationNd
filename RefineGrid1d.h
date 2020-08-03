cd603904237b97259d08d45cd22270c4938a1878
Chris Anderson <anderson@math.ucla.edu>
2020-08-03 10:00:01 -0700

    added classes to create integer refinements of SCC::GridFunctions

---
+++ RefineGrid1d.h
@@ -0,0 +1,50 @@
+/*
+ * RefineGrid1d.h
+ *
+ *  Created on: Jul 31, 2020
+ *      Author: anderson
+ */
+
+#ifndef REFINE_GRID_1D_
+#define REFINE_GRID_1D_
+
+#include "GridFunctionNd/SCC_GridFunction1d.h"
+#include "InterpolationNd/LegendreGridFunApprox1d.h"
+
+class RefineGrid1d
+{
+	public:
+
+	void refineNX(int degree, int N, SCC::GridFunction1d& F, SCC::GridFunction1d& FN)
+	{
+	    long xPanels = F.getXpanelCount();
+	    double xMin  = F.getXmin();
+	    double xMax  = F.getXmax();
+	    double   hx  = F.getHx();
+
+		interp1d.initialize(degree,xPanels,xMin,xMax,F.getDataPointer());
+
+		FN.initialize(N*xPanels,xMin,xMax);
+
+		double xPos;
+		double hFact = 1.0/(double)N;
+
+	    for(long i = 0; i < xPanels; i++)
+	    {
+	    	FN(N*i)   = F(i);
+
+	    	for(long k = 1; k < N; k++)
+	    	{
+	    	xPos = xMin + i*hx + k*hFact*hx;
+	    	FN(N*i+k) = interp1d.evaluate(xPos);
+	    	}
+	    }
+	    FN(N*xPanels) = F(xPanels);
+	}
+
+
+	LegendreGridFunApprox1d interp1d;
+};
+#endif
+
+
---
+++ RefineGrid2d.h
@@ -0,0 +1,189 @@
+/*
+ * RefineGrid2d.h
+ *
+ *  Created on: Jul 31, 2020
+ *      Author: anderson
+ */
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+
+#ifndef REFINE_GRID_2D_
+#define REFINE_GRID_2D_
+
+#include "GridFunctionNd/SCC_GridFunction2d.h"
+#include "InterpolationNd/LegendreGridFunApprox2d.h"
+
+class RefineGrid2d
+{
+	public:
+
+	void refineNX(int degreeX, int degreeY, int N, SCC::GridFunction2d& F, SCC::GridFunction2d& FN)
+	{
+	    long xPanels = F.getXpanelCount();
+	    double xMin  = F.getXmin();
+	    double xMax  = F.getXmax();
+	    double   hx  = F.getHx();
+
+
+	    long yPanels = F.getYpanelCount();
+	    double yMin  = F.getYmin();
+	    double yMax  = F.getYmax();
+	    double   hy  = F.getHy();
+
+		double xPos;
+		double yPos;
+
+#ifndef _OPENMP
+		interp2d.initialize(degreeX,xPanels,xMin,xMax,degreeY,yPanels,yMin,yMax,F.getDataPointer());
+		FN.initialize(N*xPanels,xMin,xMax,N*yPanels,yMin,yMax);
+
+		double hFact= 1.0/(double)N;
+
+	    for(long i = 0; i < xPanels; i++)
+	    {
+	   	for(long j = 0; j < yPanels; j++)
+	    {
+	    	for(long p = 0; p < N; p++)
+	    	{
+	        xPos = xMin + i*hx + p*hFact*hx;
+	        for(long q = 0; q < N; q++)
+	    	{
+	    	yPos = yMin + j*hy + q*hFact*hy;
+	    	if((p == 0)&&(q == 0))
+	    	{
+	    		FN(N*i,N*j)   = F(i,j);
+	    	}
+	    	else
+	    	{
+	    		FN(N*i+p,N*j+q) = interp2d.evaluate(xPos,yPos);
+	    	}
+
+	    	}}
+	    }}
+
+	    // Fill in top and right side
+
+	    long j = yPanels;
+	    yPos   = yMax;
+
+	    for(long i = 0; i < xPanels; i++)
+	    {
+	    FN(N*i,N*j)   = F(i,j);
+	    for(long p = 1; p < N; p++)
+	    {
+	        xPos = xMin + i*hx + p*hFact*hx;
+	        FN(N*i+p,N*j) = interp2d.evaluate(xPos,yPos);
+	    }
+	    }
+
+	    long i = xPanels;
+	    xPos   = xMax;
+
+	    for(long j = 0; j < yPanels; j++)
+	    {
+	    FN(N*i,N*j)   = F(i,j);
+	    for(long q = 1; q < N; q++)
+	    {
+	        yPos = yMin + j*hy + q*hFact*hy;
+	        FN(N*i,N*j+q) = interp2d.evaluate(xPos,yPos);
+	    }
+	    }
+
+	    // Upper right corner
+
+	    FN(N*xPanels,N*yPanels) = F(xPanels,yPanels);
+#endif
+
+#ifdef _OPENMP
+
+	    int curThreadCount = omp_get_max_threads();
+	    int tI;
+
+	    interp2dA.resize(curThreadCount);
+
+	    for(long k = 0; k < curThreadCount; k++)
+	    {
+	    	interp2dA[k].initialize(degreeX,xPanels,xMin,xMax,degreeY,yPanels,yMin,yMax,F.getDataPointer());
+	    }
+
+		FN.initialize(N*xPanels,xMin,xMax,N*yPanels,yMin,yMax);
+
+		double hFact= 1.0/(double)N;
+
+	    #pragma omp parallel for  \
+        private(xPos,yPos,tI)\
+        schedule(static,1)
+	    for(long i = 0; i < xPanels; i++)
+	    {
+        tI = omp_get_thread_num();
+	   	for(long j = 0; j < yPanels; j++)
+	    {
+
+	    	for(long p = 0; p < N; p++)
+	    	{
+	        xPos = xMin + i*hx + p*hFact*hx;
+	        for(long q = 0; q < N; q++)
+	    	{
+	    	yPos = yMin + j*hy + q*hFact*hy;
+	    	if((p == 0)&&(q == 0))
+	    	{
+	    		FN(N*i,N*j)   = F(i,j);
+	    	}
+	    	else
+	    	{
+	    		FN(N*i+p,N*j+q) = interp2dA[tI].evaluate(xPos,yPos);
+	    	}
+
+	    	}}
+	    }}
+
+	    // Fill in top and right side
+
+	    long j = yPanels;
+	    yPos   = yMax;
+
+	    for(long i = 0; i < xPanels; i++)
+	    {
+	    FN(N*i,N*j)   = F(i,j);
+	    for(long p = 1; p < N; p++)
+	    {
+	        xPos = xMin + i*hx + p*hFact*hx;
+	        FN(N*i+p,N*j) = interp2dA[0].evaluate(xPos,yPos);
+	    }
+	    }
+
+	    long i = xPanels;
+	    xPos   = xMax;
+
+	    for(long j = 0; j < yPanels; j++)
+	    {
+	    FN(N*i,N*j)   = F(i,j);
+	    for(long q = 1; q < N; q++)
+	    {
+	        yPos = yMin + j*hy + q*hFact*hy;
+	        FN(N*i,N*j+q) = interp2dA[0].evaluate(xPos,yPos);
+	    }
+	    }
+
+	    // Upper right corner
+
+	    FN(N*xPanels,N*yPanels) = F(xPanels,yPanels);
+
+
+#endif
+	}
+
+#ifndef _OPENMP
+	LegendreGridFunApprox2d interp2d;
+#endif
+
+#ifdef _OPENMP
+	std::vector<LegendreGridFunApprox2d> interp2dA;
+#endif
+};
+#endif
+
+
